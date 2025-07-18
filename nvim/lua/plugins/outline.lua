f9018ba7338e182dfe55a111bdf291178c811135
sokinpui <sosokinpui@gmail.com>
2025-07-18 14:27:52 +0800

    nvim: outliner move to left split window, add vtsls lsp

--- nvim/lua/plugins/outline.lua
+++ nvim/lua/plugins/outline.lua
@@ -1,12 +1,17 @@
 return {
-  {
-    "hedyhli/outline.nvim",
-    keys = {
-      { "<leader>ol", "<cmd>Outline<cr>", desc = "Outline" },
-    },
-    cmd = { "Outline", "OutlineOpen" },
-    config = function()
-      require("outline").setup()
-    end,
-  },
+	{
+		"hedyhli/outline.nvim",
+		keys = {
+			{ "<leader>ol", "<cmd>Outline<cr>", desc = "Outline" },
+		},
+		cmd = { "Outline", "OutlineOpen" },
+		config = function()
+			require("outline").setup({
+				outline_window = {
+					position = "left",
+					auto_jump = true,
+				},
+			})
+		end,
+	},
 }
