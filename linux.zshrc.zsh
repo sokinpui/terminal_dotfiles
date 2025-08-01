7edd3ff642af5aafffece44419ce665f68e42fc4
sokinpui <sosokinpui@gmail.com>
2025-08-01 08:44:37 +0800

    update linux.zshrc.zsh

--- linux.zshrc.zsh
+++ linux.zshrc.zsh
@@ -342,7 +342,11 @@ bindkey '^g' jump-to-dir-in-git-repo
 
 # Ctrl-T: FZF file finder (built-in)
 # This uses fzf's own keybinding script logic, which is more robust.
-source <(fzf --zsh) # This provides the widget
+
+# source <(fzf --zsh)
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+source /usr/share/doc/fzf/examples/completion.zsh
+
 # Customize fzf options
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
