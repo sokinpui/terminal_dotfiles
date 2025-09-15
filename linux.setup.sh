7ae93501703d36304e0014d199008319ec48d57b
sokinpui <sosokinpui@gmail.com>
2025-09-15 16:25:15 +0800

    fix

--- linux.setup.sh
+++ linux.setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo apt install -y curl wget git build-essential python3 python3-pip zsh tmux neovim fd-find fzf zoxide ripgrep direnv
+sudo apt install -y curl wget git build-essential python3 python3-pip zsh tmux neovim fd-find fzf zoxide ripgrep direnv pipx
 
 ln -s $(which fdfind) ~/.local/bin/fd
+
+pipx install trash-cli
+
+sudo apt remove -y fzf
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
