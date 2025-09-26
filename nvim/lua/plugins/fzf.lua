c45805d71b3ab0e4e90f4276fd5d5b417dbace0a
sokinpui <sosokinpui@gmail.com>
2025-09-26 13:56:44 +0800

    update fzf key bindings

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,6 +1,4 @@
-local opts = { silent = true, noremap = true }
 return {
-
 	{
 		"ibhagwan/fzf-lua",
 		dependencies = {
@@ -10,28 +8,28 @@ return {
 		keys = {
 			{ "<space>ff", "<cmd>FzfLua<cr>" },
 			{
+				"<C-b>",
+				function()
+					require("fzf-lua").buffers()
+				end,
+			},
+			{
 				"<C-f>",
 				function()
-					local success, result = pcall(require("fzf-lua").git_files)
-					if not result then
-						require("fzf-lua").files()
-					end
+					require("fzf-lua").files()
 				end,
 			},
 			{
-				"<C-p>",
+				"<C-g>",
 				function()
-					require("fzf-lua").live_grep_native({
-						fzf_opts = {
-							["--tiebreak"] = "index",
-						},
-					})
+					require("fzf-lua").grep({ search = "" })
 				end,
 			},
 			{
-				"<space>fl",
+				mode = "v",
+				"<C-g>",
 				function()
-					require("fzf-lua").lines()
+					require("fzf-lua").grep_visual()
 				end,
 			},
 			{
@@ -43,10 +41,10 @@ return {
 		},
 		cmd = "FzfLua",
 		config = function()
-			require("fzf-lua").setup({ "fzf-native" })
-			local actions = require("fzf-lua.actions")
+			-- require("fzf-lua").setup({ "fzf-native" })
 			-- calling `setup` is optional for customization
 			require("fzf-lua").setup({
+				"fzf-native",
 				global_resume = true,
 				global_resume_query = true,
 				file_icons = false,
@@ -57,39 +55,13 @@ return {
 						horizontal = "down:55%",
 					},
 				},
-				fzf_opts = {
-					["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-history",
-				},
 				hls = {
 					cursorline = "Search",
 					buf_name = "Normal",
 				},
-				buffers = {
-					color_icons = true,
-					sort_lastused = true,
-					show_unloaded = true,
-					cwd_only = false,
-					cwd = nil,
-					actions = {
-						["ctrl-x"] = { fn = actions.buf_del, reload = true },
-					},
-				},
-				actions = {
-					files = {
-						["default"] = actions.file_edit_or_qf,
-						["ctrl-s"] = actions.file_split,
-						["ctrl-v"] = actions.file_vsplit,
-						["ctrl-t"] = actions.file_tabedit,
-						["ctrl-q"] = actions.file_sel_to_qf,
-						["alt-l"] = actions.file_sel_to_ll,
-					},
-					buffers = {
-						["default"] = actions.buf_edit,
-						["ctrl-s"] = actions.buf_split,
-						["ctrl-v"] = actions.buf_vsplit,
-						["ctrl-t"] = actions.buf_tabedit,
-						["ctrl-q"] = actions.file_sel_to_qf,
-					},
+				files = {
+					cwd_prompt = false,
+					prompt = "Files❯ ",
 				},
 			})
 		end,
