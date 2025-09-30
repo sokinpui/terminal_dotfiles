65243a51b1cd98f8fbdf8a427dc5633fbb2a51b0
sokinpui <sosokinpui@gmail.com>
2025-09-30 11:08:32 +0800

    add quickfix list toggle

--- nvim/lua/plugins/quickfix.lua
+++ nvim/lua/plugins/quickfix.lua
@@ -17,6 +17,35 @@ return {
 			})
 			vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
 			vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>")
+
+			local function ToggleQuickFix()
+				-- Check if the quickfix window is already open
+				local qf_open = false
+				for _, win in ipairs(vim.fn.getwininfo()) do
+					if win.quickfix == 1 then
+						qf_open = true
+						break
+					end
+				end
+
+				if qf_open then
+					-- If it's open, close it
+					vim.cmd("cclose")
+				else
+					-- Otherwise, try to open it
+					-- pcall is used to catch errors, for instance, if there is no quickfix list
+					local success, _ = pcall(vim.cmd, "copen")
+					if not success then
+						print("No Quickfix list available.")
+					end
+				end
+			end
+
+			-- Map the function to <leader>qf in normal mode
+			vim.keymap.set("n", "<leader>qf", ToggleQuickFix, { desc = "Toggle Quickfix window" })
+			vim.keymap.set("n", "<C-l>", function()
+				ToggleQuickFix()
+			end)
 		end,
 	},
 	-- {
