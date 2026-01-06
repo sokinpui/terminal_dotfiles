d60f9d7620546e300ea6b50afaef45c3ad4baf84
sokinpui <sosokinpui@gmail.com>
2026-01-06 22:35:55 +0800

    updaet neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,15 @@
 vim.loader.enable()
+require("setup")
 
-local user = vim.fn.expand("$USER")
-local hostname = vim.fn.hostname()
-
--- table of user and hostname
-local allow_plugins = {
-	["dou"] = "MacBookAir",
-	["so"] = "Mac-Studio",
-}
-
-for u, h in pairs(allow_plugins) do
-	if user == u and hostname == h then
-		require("setup")
-	end
-end
+-- local user = vim.fn.expand("$USER")
+-- local hostname = vim.fn.hostname()
+-- -- table of user and hostname
+-- local allow_plugins = {
+-- 	["dou"] = "MacBookAir",
+-- 	["so"] = "Mac-Studio",
+-- }
+-- for u, h in pairs(allow_plugins) do
+-- 	if user == u and hostname == h then
+-- 		require("setup")
+-- 	end
+-- end
