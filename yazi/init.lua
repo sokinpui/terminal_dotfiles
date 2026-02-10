b8204765a84678c687852ab36cd65157ea549455
sokinpui <sosokinpui@gmail.com>
2026-02-10 12:19:47 +0800

    add plugin, finalize config
    fuck lf

---
+++ yazi/init.lua
@@ -0,0 +1,18 @@
+-- ~/.config/yazi/init.lua
+th.git = th.git or {}
+th.git.unknown_sign = "?"
+th.git.modified_sign = "M"
+th.git.added_sign = "A"
+th.git.untracked_sign = "U"
+th.git.ignored_sign = "I"
+th.git.deleted_sign = "D"
+th.git.updated_sign = "U"
+th.git.clean_sign = "C"
+
+require("git"):setup({ order = 1500 })
+
+require("no-status"):setup()
+
+require("full-border"):setup({
+	type = ui.Border.ROUNDED,
+})
