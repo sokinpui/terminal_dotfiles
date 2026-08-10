6ede81cffd7d5bd664bcf6a2fda97514ee24ee06
sokinpui <sosokinpui@gmail.com>
2026-08-10 09:02:54 +0800

    add package for mac install

--- setup.sh
+++ setup.sh
@@ -69,6 +69,7 @@ setup_macos() {
     coreutils
     cmake
     gettext
+    iproute2mac
     fd
     colima
     neofetch
@@ -84,6 +85,7 @@ setup_macos() {
     skhd
     direnv
     zoxide
+    tlrc
     yqrashawn/goku/goku
     sync-clip
   )
